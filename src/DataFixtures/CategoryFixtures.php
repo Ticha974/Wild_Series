6519bc1ab51622ea31da41d97e940a7f88f241cf
Ticha974 <natacha.crepu@hotmail.fr>
2022-01-19 21:07:23 +0100

    quest 09

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Animation',
+        'Fantastique',
+        'Horreur',
+        'Comédie',
+        'Romance',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
